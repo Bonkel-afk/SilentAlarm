366df30321dd6facc707b3f96e334b3196bea491
JasonVoigt <Zockeryolo123@gmail.com>
2026-03-06 15:49:11 +0100

    Configured the run configuration, so that everything starts with npm run dev

--- electron/main.js
+++ electron/main.js
@@ -1,17 +1,19 @@
 const { app, BrowserWindow } = require("electron")
 const path = require("path")
 
+const isDev = !app.isPackaged
+
 function createWindow() {
   const win = new BrowserWindow({
-    width: 800,
-    height: 600,
-    webPreferences: {
-      nodeIntegration: true
-    }
+    width: 1000,
+    height: 700
   })
 
-  // Lokales React Build laden
-  win.loadFile(path.join(__dirname, "client/dist/index.html"))
+  if (isDev) {
+    win.loadURL("http://localhost:5173")
+  } else {
+    win.loadFile(path.join(__dirname, "../client/dist/index.html"))
+  }
 }
 
 app.whenReady().then(createWindow)
